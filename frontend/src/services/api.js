e30f2df918131736b4e9b50392c0b227e0200287
Vedant Gupta <vedantguptajhansi18@gmail.com>
2026-01-07 21:05:08 +0530

    Update API base URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9000/api"
+  baseURL: "https://taskmanager-backend-11fy.onrender.com"
 });
 
 api.interceptors.request.use(config => {
